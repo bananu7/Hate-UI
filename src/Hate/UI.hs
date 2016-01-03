c5c6d657098a517bb4ae84b1af8d7c61374f20d7
bananu7 <bananu7@o2.pl>
2016-01-04 00:46:55 +0100

    Printing a label sorta works

--- src/Hate/UI.hs
+++ src/Hate/UI.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Hate.UI where
 
 import Hate.Graphics
 import qualified Hate.Events.Types as Hate
+import Hate.Fonts
+import Control.Monad.Reader
+import Control.Monad.State
 
 {-
 data UIEvent =
@@ -19,22 +23,44 @@ class (EventReceiver a, Show a) => Element a where
 
 -}
 
-class Element a where
-    drawElement :: a -> [DrawRequest]
-
+{-
 data ElementNode = forall a. Element a => ElementNode {
       value :: a
     , children :: [ElementNode]
 }
+-}
+
+data UI = UI { 
+    uiFont :: Font
+    --, rootElement :: ElementNode 
+}
+
+{-
+class Element a where
+    drawElement :: MonadReader UI m => a -> m [DrawRequest]
 
 instance Element ElementNode where
     drawElement (ElementNode val cs) = drawElement val ++ concatMap drawElement cs
+-}
+
+type WithUI a = forall m. MonadState UI m => m a
+type WithUIRead a = forall m. MonadReader UI m => m a
 
 -- Here come the actual controls
 data Label = Label String
 
+drawLabel :: Label -> WithUIRead [DrawRequest]
+drawLabel (Label str) = ask >>= \ui -> return $ hatePrint (uiFont ui) str
+
+--instance Element Label where
+--    drawElement (Label str) = hatePrint .. .. str
+
 -- In order to keep things simple, button cannot nest arbitrary controls
 data Button = Button Label
+--instance Element Button where
+
+
+
 --data ImageButton = ImageButton Hate.Sprite
 
 {-
--- src/main.hs
+++ src/main.hs
@@ -10,28 +10,32 @@ import Hate.UI
 
 import Control.Monad.Reader
 
-data FontConfig = FontConfig {
-    fontSprite :: Sprite,
-    fontData :: Font
-}
-
 data SampleState = SampleState {
-    fontConfig :: FontConfig
+      font :: Font
+    , label :: Label
+    , ui :: UI
 }
 
 myPrint txt = do
     s <- ask
-    return $ hatePrint (fontData . fontConfig $ s) (fontSprite . fontConfig $ s) txt
+    return $ hatePrint (font $ s) txt
 
+hoistUI :: Reader UI a -> Reader SampleState a
+hoistUI f = ask >>= \s -> return $ runReader f (ui s)
 
 sampleLoad :: LoadFn SampleState
-sampleLoad = SampleState <$>
-    (FontConfig <$> loadSprite "Arial_0.png"
-                <*> loadFont "Arial.fnt")
+sampleLoad = SampleState 
+    <$> ((,) <$> loadFontData "Arial.fnt"
+             <*> loadSprite "Arial_0.png")
+    <*> (pure $ Label "test label")
+    <*> (UI <$> ((,) <$> loadFontData "Arial.fnt"
+                     <*> loadSprite "Arial_0.png"))
 
 sampleDraw :: DrawFn SampleState
 sampleDraw = runReader $ do
     myPrint "Haters gonna Hate"
+    l <- label <$> ask
+    hoistUI $ drawLabel l
 
 sampleUpdate :: UpdateFn SampleState
 sampleUpdate _ = return ()
